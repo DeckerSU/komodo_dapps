971117ca50043feb90ae56c9bb04da68e799cd78
DeckerSU <deckersu@protonmail.com>
2020-06-10 02:46:13 +0300

    fix curl dependency "install" phase

--- build_deps.cmd
+++ build_deps.cmd
@@ -1,9 +1,10 @@
 @echo off
+set ProjectFolder=%CD%
 rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
 :compile_curl
 cd win_depends 
-rem git clone https://github.com/curl/curl
+git clone https://github.com/curl/curl
 cd curl
 call buildconf.bat
 cd winbuild
@@ -11,4 +12,4 @@ rem set RTLIBCFG=static
 nmake /f Makefile.vc mode=static vc=16 debug=no ENABLE_WINSSL=yes
 nmake /f Makefile.vc mode=static vc=16 debug=yes ENABLE_WINSSL=yes
 :install 
-echo a |xcopy /e /h /c /i "../builds/libcurl-vc16-x64-release-static-ipv6-sspi-winssl/include" "../../../includes"
+echo a |xcopy /e /h /c /i "../builds/libcurl-vc16-x64-release-static-ipv6-sspi-winssl/include" "%ProjectFolder%\includes"
